86f84e7871f78165ee5b5cf127609e8fbbd76c43
Sreya Rajendran <Sreya Rajendran@DESKTOP-O31F3P0>
2018-06-02 16:01:42 -0700

    Accept department

--- JavaProjects/src/emailapp/Email.java
+++ JavaProjects/src/emailapp/Email.java
@@ -1,5 +1,7 @@
 package emailapp;
 
+import java.util.Scanner;
+
 public class Email {
 	private String firstName;
 	private String lastName;
@@ -13,5 +15,27 @@ public class Email {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		System.out.println("Email created: " + this.firstName + " " + this.lastName);
+		
+		//Call method asking for department
+		this.department = setDepartment();
+		System.out.println("Department: " + this.department);
+	}
+	
+	private String setDepartment() {
+		System.out.println("DEPARTMENT CODES\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none\nEnter department code:");
+		Scanner in = new Scanner(System.in);
+		int depChoice = in.nextInt();
+		in.close();
+		switch(depChoice) {
+			case 1: 
+				return "sales";
+			case 2:
+				return "dev";
+			case 3:
+				return "acct";
+			default:
+				return "";
+		}
+		
 	}
 }
