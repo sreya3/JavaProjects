cca909734553528108e8e5048f0758b8b5e2454b
Sreya Rajendran <Sreya Rajendran@DESKTOP-O31F3P0>
2018-06-02 16:15:22 -0700

    Generate random 8 character password for user

--- JavaProjects/src/emailapp/Email.java
+++ JavaProjects/src/emailapp/Email.java
@@ -6,9 +6,11 @@ public class Email {
 	private String firstName;
 	private String lastName;
 	private String password;
+	private int defaultPasswordLength = 8;
 	private String department;
 	private int mailboxCapacity;
 	private String alternateEmail;
+	
 	
 	//Constructor
 	public Email(String firstName, String lastName) {
@@ -19,8 +21,13 @@ public class Email {
 		//Call method asking for department
 		this.department = setDepartment();
 		System.out.println("Department: " + this.department);
+		
+		//call method to generate random password
+		this.password = randomPassword(defaultPasswordLength);
+		System.out.println("Your Password: " + this.password);
 	}
 	
+	//ask for department
 	private String setDepartment() {
 		System.out.println("DEPARTMENT CODES\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none\nEnter department code:");
 		Scanner in = new Scanner(System.in);
@@ -37,5 +44,16 @@ public class Email {
 				return "";
 		}
 		
+	}
+	
+	//generate random password
+	private String randomPassword(int length) {
+		String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890$#@%";
+		char[] password = new char[length];
+		for(int i = 0; i < length; i++) {
+			int rand = (int)(Math.random() * passwordSet.length());
+			password[i] = passwordSet.charAt(rand);
+		}
+		return new String(password);
 	}
 }
