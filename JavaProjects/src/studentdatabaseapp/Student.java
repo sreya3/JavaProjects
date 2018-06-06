ee78cf7deded5fcf093450b7712cc26c84b39d44
Sreya Rajendran <Sreya Rajendran@DESKTOP-O31F3P0>
2018-06-05 19:18:42 -0700

    Generate and set id for each student

--- JavaProjects/src/studentdatabaseapp/Student.java
+++ JavaProjects/src/studentdatabaseapp/Student.java
@@ -6,11 +6,11 @@ public class Student {
 	private String firstName;
 	private String lastName;
 	private int gradeYear;
-	private int studentId;
+	private String studentId;
 	private String courses;
 	private int tuitionBalance;
 	private static int courseCost = 600;
-	private static int id = 1001;
+	private static int id = 1000;
 	public Student() {
 		Scanner in = new Scanner(System.in);
 		System.out.println("Enter student first name:");
@@ -22,10 +22,16 @@ public class Student {
 		System.out.println("1 - Freshman\n2 - Sophmore\n3 - Junior\n4 - Senior\nEnter student class level:");
 		this.gradeYear = in.nextInt();
 		
-		System.out.println(firstName + " " + lastName + " " + gradeYear);
-		
+		setStudentId();
+		System.out.println(firstName + " " + lastName + " " + gradeYear + " " + studentId);
 		
 	}
 	
+	//generate an ID
+	private void setStudentId() {
+		id++;
+		//Grade level + id
+		this.studentId = gradeYear + "" + id;
+	}
 	
 }
